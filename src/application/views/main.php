0d804c74e1faf72d2bb4b6448f2a4f89376f1ded
CrazyYakov <Gitarar98@yandex.ru>
2021-07-04 20:12:14 +0500

    0.3.0 updated view main host

--- src/application/views/main.php
+++ src/application/views/main.php
@@ -1,9 +1,13 @@
+<?php
+
+$this->page = "Главное меню";
+
+?>
+
 <div class="container">
     <div class="list-group w-25 mt-5">
-        <a href="/user/index" class="list-group-item list-group-item-action" > Пользователи </a>
-        <a href="/department/index" class="list-group-item list-group-item-action" > Департамент </a>
-        <a href="/job/index" class="list-group-item list-group-item-action" > список работ </a>
+        <a href="/user/index" class="list-group-item list-group-item-action"> Пользователи </a>
+        <a href="/department/index" class="list-group-item list-group-item-action"> Департамент </a>
+        <a href="/job/index" class="list-group-item list-group-item-action"> список работ </a>
     </div>
 </div>
-
-
